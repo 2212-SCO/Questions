b596551bc057ef00d65f815bdb3e8115e6e50e92
George Khoury <gke@sent.com>
2023-02-24 14:54:43 -0800

    disabled redis cache

--- server/controller/index.js
+++ server/controller/index.js
@@ -6,21 +6,30 @@ module.exports = {
   getQuestions(req, res) {
     let page = req.query.page || 1;
     let count = req.query.count || 5;
-    let redisKey = req.query.product_id.toString() + 'page' + page.toString() + 'count' + count.toString();
-    redis.get(redisKey)
-      .then(data => {
-        if(data) {
-          console.log('information sent from redis');
-          res.status(200).json(JSON.parse(data)).send()
-        } else {
-          model.getQuestions(req.query.product_id, count, page)
-            .then((response) => {
-              redis.set(redisKey, JSON.stringify(response));
-              res.status(200).json(response).send();
-            })
-          .catch(err =>  res.status(500).send(err))
-        }
+    model.getQuestions(req.query.product_id, count, page)
+      .then((response) => {
+        res.status(200).json(response).send();
       })
+    .catch(err =>  res.status(500).send(err))
+    // uncomment this to add redis cache
+    // let redisKey = req.query.product_id.toString() + 'page' + page.toString() + 'count' + count.toString();
+    // redis.get(redisKey)
+    //   .then(data => {
+    //     if(data) {
+    //       console.log('information sent from redis');
+    //       res.status(200).json(JSON.parse(data)).send()
+    //     } else {
+    //       model.getQuestions(req.query.product_id, count, page)
+    //         .then((response) => {
+    //           redis.set(redisKey, JSON.stringify(response));
+    //           res.status(200).json(response).send();
+    //         })
+    //       .catch(err =>  res.status(500).send(err))
+    //     }
+    //   })
+
+
+
     //   .catch(err => console.log(err));
     //   res.status(200).json(redis.get(redisKey)).send()
     // } else {
