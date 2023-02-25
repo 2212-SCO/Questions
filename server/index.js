d6324510433ea671d7f646153286813fefa4e68e
George Khoury <gke@sent.com>
2023-02-24 16:50:25 -0800

    added loader io token

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const router = require('./routes.js');
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router);
 
 const PORT = process.env.PORT || 3000;
